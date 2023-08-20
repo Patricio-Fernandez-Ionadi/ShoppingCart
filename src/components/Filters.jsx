888d3d9b1aca1bd78017260f7689202cbd934379
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2023-08-20 02:15:20 -0300

    hook useId

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,47 +1,51 @@
 /* eslint-disable react/prop-types */
-import { useState } from 'react'
+import { useId, useState } from 'react'
 import './Filters.css'
 
 export function Filters({onChange}) {
 
-    const [minPrice, setMinPrice] = useState(0)
+  const [minPrice, setMinPrice] = useState(0)
 
-    const handleChangeMinPrice = () => {
-        setMinPrice(event.target.value)
-        onChange(prevState => ({
-            ...prevState,
-            minPrice: event.target.value
-        }))
-    }
+	const minPriceFilterId = useId()
+	const categoryFilterId = useId()
 
-    const hanldleChangeCategory = () => {
-        onChange(prevState => ({
-            ...prevState,
-            category: event.target.value
-        }))
-    }
+  const handleChangeMinPrice = () => {
+    setMinPrice(event.target.value)
+    onChange(prevState => ({
+      ...prevState,
+      minPrice: event.target.value
+    }))
+  }
+
+  const hanldleChangeCategory = () => {
+    onChange(prevState => ({
+      ...prevState,
+      category: event.target.value
+    }))
+  }
 
 
   return (
     <>
-        <section className='filters'>
+      <section className='filters'>
 
-            <div>
-                <label htmlFor="price">Precio a partir de:</label>
-                <input onChange={handleChangeMinPrice} type="range" name="price" id="price" min={0} max={1000} />
-                <span>${minPrice}</span>
-            </div>
+        <div>
+            <label htmlFor="price">Precio a partir de:</label>
+            <input onChange={handleChangeMinPrice} type="range" name="price" id={minPriceFilterId} min={0} max={1000} />
+            <span>${minPrice}</span>
+        </div>
 
-            <div>
-                <label htmlFor="category">Categoria</label>
-                <select name="category" id="category" onChange={hanldleChangeCategory}>
-                    <option value="all">Todas</option>
-                    <option value="laptops">Prtatiles</option>
-                    <option value="smartphones">Celulares</option>
-                </select>
-            </div>
 
-        </section>
+        <div>
+            <label htmlFor="category">Categoria</label>
+            <select name="category" id={categoryFilterId} onChange={hanldleChangeCategory}>
+                <option value="all">Todas</option>
+                <option value="laptops">Prtatiles</option>
+                <option value="smartphones">Celulares</option>
+            </select>
+        </div>
+
+      </section>
     </>
   )
 }
