2ea86d7db856c38222a064442098626a16f0f35a
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2023-08-20 19:10:54 -0300

    sintaxis

--- src/hooks/useFilters.js
+++ src/hooks/useFilters.js
@@ -1,21 +1,17 @@
-import { useContext } from "react"
-import { FiltersContext } from "../context"
+import { useContext } from "react";
+import { FiltersContext } from "../context";
 
 export function useFilters() {
+	const { filters, setFilters } = useContext(FiltersContext);
 
-  const {filters, setFilters} = useContext(FiltersContext)
+	const filterProducts = (products) => {
+		return products.filter((prod) => {
+			return (
+				prod.price >= filters.minPrice &&
+				(filters.category === "all" || prod.category === filters.category)
+			);
+		});
+	};
 
-  const filterProducts = (products) => {
-    return products.filter(prod => {
-      return (
-        prod.price >= filters.minPrice &&
-        (
-          filters.category === 'all' ||
-          prod.category === filters.category
-        )
-      )
-    })
-  }
-
-  return {filters, filterProducts, setFilters}
+	return { filters, filterProducts, setFilters };
 }
